a8617eb61f8cf672cb207524eab1e11d4df094c7
Zeke Pari <pariezekiel@gmail.com>
2025-02-13 00:24:34 +1000

    style: adjust Navbar layout for improved responsiveness and spacing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,14 +6,14 @@ export default function Navbar() {
   return (
     <div className="bg-base-100 shadow-sm">
       <div className="navbar container">
-        <div className="navbar-start">
+        <div className="navbar-start max-lg:gap-2">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <Menu size={24} />
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
                 <Link href="/">About</Link>
